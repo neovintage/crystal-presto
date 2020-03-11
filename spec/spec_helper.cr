e3782f1795cc68758d0e9176331c42938fa163c7
neovintage <neovintage@gmail.com>
2020-03-11 00:17:38 -0700

    Getting GitHub Actions to actually work

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,4 +1,7 @@
 require "spec"
 require "../src/presto"
 
-DB_URL = ENV["DATABASE_URL"]? || "presto://presto:@localhost:8080/tpch/sf1"
+DB_URL = ENV["DATABASE_URL"]? ||
+          ENV["PRESTO_HOST"]? && ENV["PRESTO_PORT"]? ? "presto://presto:@#{ENV["PRESTO_HOST"]}:#{ENV["PRESTO_PORT"]}/tpch/sf1" : "presto://presto:@localhost:8080/tpch/sf1"
+
+puts DB_URL
