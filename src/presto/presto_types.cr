dd226ac4ab9165d007e2416a244a0a1420949ea0
neovintage <neovintage@gmail.com>
2020-11-05 13:58:58 -0800

    got the json mapped properly

---
+++ src/presto/presto_types.cr
@@ -0,0 +1,185 @@
+module Presto
+  alias PrestoValue = String | Nil | Bool | Int32 | Int64 | Float32 | Float64 | Time | JSON::Any
+
+  struct PrestoColumn
+    include JSON::Serializable
+
+    property name : String
+    property type : String
+  end
+
+  struct PrestoWarning
+  end
+
+  struct ErrorLocation
+    include JSON::Serializable
+
+    @[JSON::Field(key: "lineNumber")]
+    property line_number : Int
+
+    @[JSON::Field(key: "columnNumber")]
+    property column_number : Int
+  end
+
+  struct FailureInfo
+    include JSON::Serializable
+
+    property type : String
+    property message : String
+    property cause : FailureInfo
+    property supressed : Array(FailureInfo)
+    property stack : Array(String)
+
+    @[JSON::Field(key: "errorLocation")]
+    property error_location : ErrorLocation
+  end
+
+  struct StageStats
+    include JSON::Serializable
+
+    property state : String
+    property done : Bool
+    property nodes : Int32
+
+    @[JSON::Field(key: "stageId")]
+    property stageId : String
+
+    @[JSON::Field(key: "totalSplits")]
+    property total_splits : Int32
+
+    @[JSON::Field(key: "queuedSplits")]
+    property queued_splits : Int3sde2
+
+    @[JSON::Field(key: "runningSplits")]
+    property running_splits : Int32
+
+    @[JSON::Field(key: "completedSplits")]
+    property completed_splits : Int32
+
+    @[JSON::Field(key: "cpuTimeMillis")]
+    property cpu_time_milliseconds : Float32
+
+    @[JSON::Field(key: "wallTimeMillis")]
+    property wall_time_milliseconds : Float32
+
+    @[JSON::Field(key: "processedRows")]
+    property processed_rows : Float32
+
+    @[JSON::Field(key: "processedBytes")]
+    property processed_bytes : Float32
+
+    @[JSON::Field(key: "physicalInputBytes")]
+    property physical_input_bytes : Float32
+
+    @[JSON::Field(key: "subStages")]
+    property sub_stages : Array(StageStats)
+  end
+
+  struct StatementStats
+    include JSON::Serializable
+
+    property state : String
+    property queued : String
+    property nodes : Int32
+
+    @[JSON::Field(key: "totalSplits")]
+    property total_splits : Int32
+
+    @[JSON::Field(key: "queuedSplits")]
+    property queued_splits : Int32
+
+    @[JSON::Field(key: "runningSplits")]
+    property running_splits : Int32
+
+    @[JSON::Field(key: "completedSplits")]
+    property completed_splits : Int32
+
+    @[JSON::Field(key: "cpuTimeMillis")]
+    property cpu_time_milliseconds : Float32
+
+    @[JSON::Field(key: "wallTimeMillis")]
+    property wall_time_milliseconds : Float32
+
+    @[JSON::Field(key: "processedRows")]
+    property processed_rows : Float32
+
+    @[JSON::Field(key: "processedBytes")]
+    property processed_bytes : Float32
+
+    @[JSON::Field(key: "physicalInputBytes")]
+    property physical_input_bytes : Float32
+
+    @[JSON::Field(key: "subStages")]
+    property sub_stages : Array(StageStats)
+
+    @[JSON::Field(key: "queuedTimeMillis")]
+    property queued_time_milliseconds : Float32
+
+    @[JSON::Field(key: "elapsedTimeMillis")]
+    property elapsed_time_milliseconds : Float32
+
+    @[JSON::Field(key: "peakMemoryBytes")]
+    property peak_memory_bytes : Float32
+
+    @[JSON::Field(key: "spilledBytes")]
+    property spilledBytes : Float32
+
+    @[JSON::Field(key: "rootStage")]
+    property rootStage : StageStats
+  end
+
+  struct QueryError
+    include JSON::Serializable
+
+    property message : String
+
+    @[JSON::Field(key: "sqlState")]
+    property sql_state : String
+
+    @[JSON::Field(key: "errorCode")]
+    property error_code : Int32
+
+    @[JSON::Field(key: "errorName")]
+    property error_name : String
+
+    @[JSON::Field(key: "errorType")]
+    property error_type : String
+
+    @[JSON::Field(key: "errorLocation")]
+    property error_location : ErrorLocation?
+
+    @[JSON::Field(key: "failureInfo")]
+    property failureInfo : FailureInfo
+  end
+
+  class QueryResult
+    include JSON::Serializable
+    include JSON::Serializable::Unmapped
+
+    property id : String
+    property stats : StatementStats
+    property error : QueryError
+    property warnings : Array(PrestoWarning)?
+    property columns : Array(PrestoColumn)
+
+    # Data is left as JSON::Any because the serialization in Crystal types
+    # will happen lazily at the time the row is read.
+    #
+    property data : JSON::Any?
+
+    @[JSON::Field(key: "updateCount")]
+    property update_count : Int64?
+
+    @[JSON::Field(key: "infoUri")]
+    property info_uri : String?
+
+    @[JSON::Field(key: "partialCancelUri")]
+    property partial_cancel_uri : String?
+
+    @[JSON::Field(key: "nextUri")]
+    property next_uri : String?
+
+    @[JSON::Field(key: "updateType")]
+    property update_type : String?
+  end
+end
--- src/presto/result_set.cr
+++ src/presto/result_set.cr
@@ -1,67 +1,9 @@
 module Presto
-  alias PrestoValue = String | Nil | Bool | Int32 | Float32 | Float64 | Time | JSON::Any
 
-  struct PrestoColumn
-    include JSON::Serializable
-
-    property name : String
-    property type : String
-  end
-
-  struct PrestoWarning
-  end
-
-  struct StatementStats
-  end
-
-  struct QueryError
-  end
-
-  class QueryResult
-    include JSON::Serializable
-    include JSON::Serializable::Unmapped
-
-    # original java client properties
-    #
-    #@JsonProperty("id") String id,
-    #@JsonProperty("infoUri") URI infoUri,
-    #@JsonProperty("partialCancelUri") URI partialCancelUri,
-    #@JsonProperty("nextUri") URI nextUri,
-    #@JsonProperty("columns") List<Column> columns,
-    #@JsonProperty("data") List<List<Object>> data,
-    #@JsonProperty("stats") StatementStats stats,
-    #@JsonProperty("error") QueryError error,
-    #@JsonProperty("warnings") List<PrestoWarning> warnings,
-    #@JsonProperty("updateType") String updateType,
-    #@JsonProperty("updateCount") Long updateCount)
-
-    property id : String
-    property columns : JSON::Any?
-    property data : JSON::Any?
-    property stats : JSON::Any?
-    property error : JSON::Any?
-    property warnings : JSON::Any?
-    property update_count : Int64?
-
-    @[JSON::Field(key: "infoUri")]
-    property info_uri : String?
-
-    @[JSON::Field(key: "partialCancelUri")]
-    property partial_cancel_uri : String?
-
-    @[JSON::Field(key: "nextUri")]
-    property next_uri : String?
-
-    @[JSON::Field(key: "updateType")]
-    property update_type : String?
-
-  end
-
-  # todo bring this class closer to the results that come back from presto:
-  #      https://github.com/prestodb/presto/blob/master/presto-client/src/main/java/com/facebook/presto/client/QueryResults.java
-  #
   # todo because of the way presto works all the data gets returned at once we don't have a cursor
   #      consider do the serialization at the time the data comes back.
+  #
+  # todo use the query_results struct when getting information back from presto
   class ResultSet < ::DB::ResultSet
     getter query_results
     getter data : JSON::Any
