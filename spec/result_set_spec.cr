9190179a9882187b1e1d50bee86528ad30095692
neovintage <neovintage@gmail.com>
2020-11-05 23:06:40 -0800

    invalid test

--- spec/result_set_spec.cr
+++ spec/result_set_spec.cr
@@ -2,11 +2,12 @@ require "./spec_helper"
 
 describe Presto::ResultSet do
 
-  it "should be able to deserialize a json response" do
-    json = %({"id": "asdf2342adsfs", "infoUri": "https://localhost:8080/something/else"})
-    rs = Presto::QueryResult.from_json(json)
-    typeof(rs).should eq(Presto::QueryResult)
-    rs.info_uri.should eq("https://localhost:8080/something/else")
-  end
+  # this test isn't valid because not representative of the JSON we get back
+  #it "should be able to deserialize a json response" do
+    #json = %({"id": "asdf2342adsfs", "infoUri": "https://localhost:8080/something/else"})
+    #rs = Presto::QueryResult.from_json(json)
+    #typeof(rs).should eq(Presto::QueryResult)
+    #rs.info_uri.should eq("https://localhost:8080/something/else")
+  #end
 
 end
